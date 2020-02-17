e184fec754b408c89edac8e7434cb4fcd5c7db74
Russel Hussain <48565081+russel07@users.noreply.github.com>
2020-02-17 16:01:08 +0600

    some more updated

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
   echo 'hello world!!!';
   echo '<br/> File has been modified';
+$servername = "db4free.net";
+$username = "russel0507";
+$password = "RusselBd12@2020";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
